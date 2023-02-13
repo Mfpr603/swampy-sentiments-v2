05eb066494e74b9a42715653bb1bf45761308ad2
Aisha Husseini <ayasha.husseini@gmail.com>
2023-02-13 13:55:12 -0500

    trying to push to Heroku, added minor changes

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import Login from './components/Login';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
