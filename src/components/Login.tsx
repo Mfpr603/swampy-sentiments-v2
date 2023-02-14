6f77c9d538317fa04e19888682bc22ab3e1fed10
Aisha Husseini <ayasha.husseini@gmail.com>
2023-02-13 20:02:28 -0500

    fixed login Hook error and made password input protected

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,73 +1,66 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { createUserWithEmailAndPassword, onAuthStateChanged, User, signInWithEmailAndPassword, updateProfile } from 'firebase/auth';  
 import { auth } from '../firebase';
 import { useNavigate } from 'react-router-dom';
 
+
 function UserAuth() {
-  // State to store the user
-  const [user, setUser] = useState<User | null>(null);
 
-  // Use useNavigate hook to navigate between pages
-  const navigate = useNavigate();
-
-  // Use useEffect hook to listen for changes in the authentication state
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-      setUser(currentUser);
-    });
-    return () => unsubscribe();
-  }, [auth]);
-
-  // If user is authenticated, navigate to Home page
-  if (user) {
-    navigate('/Home');
-    return null;
-  }
-
-  // State to store the email and password for registration
-  const [registerEmail, setRegisterEmail] = useState("");
-  const [registerPassword, setRegisterPassword] = useState("");
-  const [registerDisplayName, setRegisterDisplayName] = useState<string>("");
-
-  // State to store the email and password for login
-  const [loginEmail, setLoginEmail] = useState("");
-  const [loginPassword, setLoginPassword] = useState("");
-
-  // Function to handle the registration
-  const register = async () => {
-    try {
-      const userCredential = await createUserWithEmailAndPassword(
-        auth, 
-        registerEmail, 
-        registerPassword 
-      );
-      const user = userCredential.user;
-      await updateProfile(user, {
-        displayName: registerDisplayName
-      });
-      console.log(user);
-    } catch (error) {
-      console.log((error as Error).message);
-    }
-  }; 
-
-  // Function to handle the login
-  const login = async () => {
-    try {
-      const user = await signInWithEmailAndPassword(
-        auth, 
-        loginEmail, 
-        loginPassword 
-      );
-      console.log(user);
-      navigate('/Home');
-    } catch (error) {
-      console.log((error as Error).message);
-    }
-  };
+    const [registerEmail, setRegisterEmail] = useState("");
+    const [registerPassword, setRegisterPassword] = useState("");
+    const [loginEmail, setLoginEmail] = useState("");
+    const [loginPassword, setLoginPassword] = useState("");
+    const [registerDisplayName, setRegisterDisplayName] = useState<string>("");
+    const navigate = useNavigate();
 
 
-  
+    const [user, setUser] = useState<User | null>(null);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+          setUser(currentUser);
+        });
+        return () => unsubscribe();
+      }, [auth]);
+    
+      // If user is authenticated, navigate to Home page
+      if (user) {
+        navigate('/Home');
+        return null;
+      }
+
+   
+
+    const register = async () => {
+        try{
+            const userCredential = await createUserWithEmailAndPassword( 
+                auth, 
+                registerEmail, 
+                registerPassword 
+            );
+            const user = userCredential.user;
+            await updateProfile(user, {
+                displayName: registerDisplayName});
+            console.log(user);
+        } catch (error) {
+            console.log((error as Error).message);
+        }
+    }; 
+
+    const login = async () => {
+        try {
+            const user = await signInWithEmailAndPassword(
+                auth, 
+                loginEmail, 
+                loginPassword 
+            );
+            console.log(user);
+            navigate('/home');
+        } catch (error) {
+            console.log((error as Error).message);
+        }
+    };
+
     return (
         <div className="App">
             <div>
@@ -78,7 +71,8 @@ function UserAuth() {
                         setRegisterEmail(event.target.value);
                     }}
                 />
-                <input
+                <input type="password"
+                    id="password"
                     placeholder="Password..."
                     onChange={(event) => {
                         setRegisterPassword(event.target.value);
@@ -103,6 +97,8 @@ function UserAuth() {
                     }}
                 />
                 <input
+                    type="password"
+                    id="password"
                     placeholder="Password..."
                     onChange={(event) => {
                         setLoginPassword(event.target.value);
